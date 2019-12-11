8c023d042db342beb23563e7b51f04a2f7052ced
Oscar Osses <oscar.osses@chilecompra.cl>
2019-12-11 19:01:11 -0300

    Updates

---
+++ precache-manifest.dfdeb03da22d05b3904693797085ed79.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11e674ed21729edc7f2991036ae295a4",
+    "url": "/desafiolatam-agenda/index.html"
+  },
+  {
+    "revision": "322b43e6d94b846c196f",
+    "url": "/desafiolatam-agenda/static/js/2.48a7a3bc.chunk.js"
+  },
+  {
+    "revision": "1ea423415ec34ed26d4a",
+    "url": "/desafiolatam-agenda/static/js/main.0bc410cd.chunk.js"
+  },
+  {
+    "revision": "02b97cd611e1b5e7fe1f",
+    "url": "/desafiolatam-agenda/static/js/runtime-main.73586144.js"
+  }
+]);
